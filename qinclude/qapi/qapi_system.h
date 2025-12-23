131ddf3bda4fbc5f50dc1d37a9a83255ab517e69
Ronghua Zeng <ronghuaz@qti.qualcomm.com>
2025-12-23 19:52:35 +0800

    hal/qcom: qwifi: add get boot reason API
    Add platform/qapi_system.c to the qwifi library to
    provide get boot reason API required by the Qualcomm
    WiFi HAL. Integrate the new source file into the
    build via zephyr_library_sources() in
    modules/hal/qcom/qwifi/CMakeLists.txt.
    
    Change-Id: I29d9822632aaf265706520317ee61aa55721b698

---
+++ qinclude/qapi/qapi_system.h
@@ -0,0 +1,47 @@
+/*
+#Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+#SPDX-License-Identifier: BSD-3-Clause
+ */
+/**
+ * @file qapi_system.h
+ *
+ */
+
+#ifndef __QAPI_SYSTEM_H__
+#define __QAPI_SYSTEM_H__
+
+/*-------------------------------------------------------------------------
+ * Include Files
+ *-----------------------------------------------------------------------*/
+#include "qapi_status.h"
+
+/*-------------------------------------------------------------------------
+ * Type Declarations
+ *-----------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+ * Boot reason types
+ *-----------------------------------------------------------------------*/
+typedef enum
+{
+    QAPI_BOOT_REASON_COLD_BOOT = 0,
+    QAPI_BOOT_REASON_DTIM_SLEEP = 1,
+    QAPI_BOOT_REASON_DEEP_SLEEP = 2,
+    QAPI_BOOT_REASON_UNKNOWN = 3
+} qapi_boot_reason_t;
+
+
+/*-------------------------------------------------------------------------
+ * Function Declarations
+ *-----------------------------------------------------------------------*/
+
+
+/**
+ *  Gets boot reason.
+ *
+ * @param[out] reason  Pointer to a qapi_boot_reason_t to receive the boot reason.
+ *
+ * @return #QAPI_OK on success, or #QAPI_ERROR if reason is NULL.
+ */
+qapi_Status_t qapi_core_obtain_boot_reason(qapi_boot_reason_t *reason);
+
+#endif /* __QAPI_SYSTEM_H__ */
