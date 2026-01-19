c6d2eb46d151a7684d9f4e6218a6af84472661e4
wenwshen <wenwshen@qti.qualcomm.com>
2026-01-19 17:45:15 +0800

    hal: wrap bmps qapi
    Change-Id: I20b963dcd1f25ca214451bb3c258b8836604d512

--- qpower/src/qapi_lowpower.c
+++ qpower/src/qapi_lowpower.c
@@ -48,3 +48,35 @@ qapi_Status_t qapi_bmps_compress_qos_null_enable(uint8_t enable)
     pdata->enable = enable;
     wmi_cmd_send(WMI_BMPS_CMPR_QOS_NULL_ENABLE_CMDID, pdata, sizeof(*pdata));
 }
+
+/**
+   @brief Config and enable/disable BMPS.
+
+   The API config and enable/disable BMPS.
+
+   @param[in] enable          1: Enable; 0: disable;
+   @param[in] idle_timeout  Idle timeout value in ms. When BMPS is enabled, system would start a timer with idle_timeout as timeout value, after timer expires, it'll check tx/rx cnt during this period, if meet condition then trigger system entering into BMPS, otherwise re-start the idle timer again;
+
+   @return
+   - QAPI_OK                             --  BMPS cfg and enable/disable successfully.
+*/
+qapi_Status_t qapi_bmps_cfg(uint8_t enable, uint32_t idle_timeout)
+{
+    if (enable != 0 && enable != 1 && enable != 2) {
+        return QAPI_ERR_INVALID_PARAM;
+    }
+    if (idle_timeout) {
+        WMI_BMPS_IDLE_TIME *pdata = (WMI_BMPS_IDLE_TIME *)&g_lowpower_wmi.bmps_cfg.bmps_idle_time;
+        memset(pdata, 0, sizeof(*pdata));
+        pdata->time = idle_timeout;
+        wmi_cmd_send(WMI_STA_IDLE_TIMER_CMDID, pdata, sizeof(*pdata));
+    }
+
+    if (enable != 2) {
+        WMI_BMPS_ENABLE *pbmps = (WMI_BMPS_ENABLE *)&g_lowpower_wmi.bmps_cfg.bmps_enable;
+        memset(pbmps, 0, sizeof(*pbmps));
+        pbmps->enable = enable;
+        wmi_cmd_send(WMI_BMPS_ENABLE_CMDID, pbmps, sizeof(*pbmps));
+    }
+    return QAPI_OK;
+}
