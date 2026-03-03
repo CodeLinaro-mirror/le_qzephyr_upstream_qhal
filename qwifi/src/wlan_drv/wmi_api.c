8454099489e94d04d62370ff3053f4310be92e1b
yonwen <yonwen@qti.qualcomm.com>
2026-03-03 19:00:26 +0800

    WLAN: Add BSSID deduplication to prevent duplicate scan results
    Change-Id: Iccaf72212ae419294f2de7ed1108d9ebe4da3d73

--- qwifi/src/wlan_drv/wmi_api.c
+++ qwifi/src/wlan_drv/wmi_api.c
@@ -304,23 +304,26 @@ static void wmi_scan_result_event(void *msg)
     qapi_WLAN_Scan_Comp_Evt_t *scan_comp_evt = (qapi_WLAN_Scan_Comp_Evt_t *)p_cxt->pScanOut;
     if (scan_comp_evt->total_bss < p_cxt->scanBssMaxCount) {
         last_idx = scan_comp_evt->num_bss_cur;
-        num_entries = scan_comp_evt->num_bss_cur + p_scan_result->num_entries;
-
-        if (num_entries > p_cxt->scanBssMaxCount) {
-            scan_comp_evt->num_bss_cur = p_cxt->scanBssMaxCount;
-        } else {
-            scan_comp_evt->num_bss_cur = num_entries;
+        int cur_idx = last_idx;
+        for (int i = 0; i < p_scan_result->num_entries && cur_idx < p_cxt->scanBssMaxCount; ++i) {
+            int duplicate = 0;
+            for (int j = 0; j < last_idx; ++j) {
+                if (memcmp(scan_comp_evt->scan_bss_info[j].bssid,
+                        p_scan_result->scan_bss_info[i].bssid,
+                        IEEE80211_ADDR_LEN) == 0) {
+                    duplicate = 1;
+                    break;
+                }
+            }
+            if (!duplicate) {
+                _wlan_fill_scan_info(&scan_comp_evt->scan_bss_info[cur_idx],
+                     (ap_info *)((unsigned char *)p_scan_result + offsetof(SCAN_RESULT, scan_bss_info) + sizeof(ap_info) * i));
+                cur_idx++;
+            }
         }
-        scan_comp_evt->scan_id = p_scan_result->scan_id;
-        int i;
-        for (i = last_idx; i < scan_comp_evt->num_bss_cur; i++) {
-            _wlan_fill_scan_info(&scan_comp_evt->scan_bss_info[i],
-                                 (ap_info *)((unsigned char *)p_scan_result + offsetof(SCAN_RESULT, scan_bss_info) +
-                                             sizeof(ap_info) * (i - last_idx)));
-        }
-    }
-    scan_comp_evt->total_bss += p_scan_result->num_entries;
-
+        scan_comp_evt->num_bss_cur = cur_idx;
+	}
+	scan_comp_evt->total_bss = scan_comp_evt->num_bss_cur;
     qurt_mutex_unlock(p_cxt->wlan_qapi_cxt_mutex);
 }
 
